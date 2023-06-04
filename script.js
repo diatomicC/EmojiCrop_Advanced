45d96531f33ac081476f4115eb889dbddd37fb26
Juhee Hur <emily.hur.21@gmail.com>
2023-06-04 13:41:55 +0800

    add more croppers- band, whatsapp, line

--- script.js
+++ script.js
@@ -63,10 +63,10 @@ function handleFileDrop(files) {
           // zip 파일 만들기
           let zip = new JSZip();
           for (let k = 0; k < slices.length; k++) {
-            zip.file("kakao/" + (k + 1) + ".png", slices[k].split(",")[1], { base64: true });
+            zip.file((k + 1) + ".png", slices[k].split(",")[1], { base64: true });
           }
           zip.generateAsync({ type: "blob" }).then(function (blob) {
-            saveAs(blob, "카카오톡(1).zip");
+            saveAs(blob, "kakaotalk_sticker.zip");
           });
         }
       };
@@ -117,18 +117,126 @@ function handleFileDrop(files) {
           // zip 파일 만들기
           let zip = new JSZip();
           for (let k = 0; k < slices.length; k++) {
-            zip.file("ogq/" + (k + 1) + ".png", slices[k].split(",")[1], { base64: true });
+            zip.file((k + 1) + ".png", slices[k].split(",")[1], { base64: true });
           }
           zip.generateAsync({ type: "blob" }).then(function (blob) {
-            saveAs(blob, "네이버OGQ(1).zip");
+            saveAs(blob, "OGQ_sticker.zip");
           });
         }
       };
     }
   });
 
-  // 두번째 카카오톡 버튼 클릭 시
-  document.querySelector("#kakao-btn2").addEventListener("click", function () {
+  // 라인 스티커 클릭 시
+  document.querySelector("#line-btn").addEventListener("click", function () {
+    // 파일 나누기
+    let slices = [];
+    for (let i = 0; i < files.length; i++) {
+      let file = files[i];
+      let img = document.createElement("img");
+      img.src = URL.createObjectURL(file);
+      img.onload = function () {
+        let canvas = document.createElement("canvas");
+        canvas.width = img.width;
+        canvas.height = img.height;
+        let ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height);
+      
+        let sliceWidth = img.width / 4;
+        let sliceHeight = img.height / 4;
+      
+        for (let i = 0; i < 4; i++) {
+          for (let j = 0; j < 4; j++) {
+            let sliceCanvas = document.createElement("canvas");
+            sliceCanvas.width = 320;
+            sliceCanvas.height = 270;
+            let sliceCtx = sliceCanvas.getContext("2d");
+            // Add transparent pixels to the left and right of each slice
+            sliceCtx.fillStyle = "rgba(0, 0, 0, 0)";
+            sliceCtx.fillRect(0, 0, 25, 270);
+            sliceCtx.fillRect(295, 0, 25, 270);
+            sliceCtx.drawImage(canvas, j * sliceWidth, i * sliceHeight, sliceWidth, sliceHeight, 25, 0, 270, 270);
+            slices.push(sliceCanvas.toDataURL("image/png"));
+          }
+        }
+      
+        // Add a 740*640px PNG file to the slices array
+        let fullCanvas = document.createElement("canvas");
+        fullCanvas.width = 320;
+        fullCanvas.height = 270;
+        let fullCtx = fullCanvas.getContext("2d");
+        fullCtx.drawImage(canvas, 0, 0, canvas.width, canvas.height, 25, 0, 270, 270);
+      
+        if (slices.length == 16) {
+          // zip 파일 만들기
+          let zip = new JSZip();
+          for (let k = 0; k < slices.length; k++) {
+            zip.file((k + 1) + ".png", slices[k].split(",")[1], { base64: true });
+          }
+          zip.generateAsync({ type: "blob" }).then(function (blob) {
+            saveAs(blob, "line_sticker.zip");
+          });
+        }
+      };
+    }
+  });
+
+    // 네이버 밴드 클릭 시
+    document.querySelector("#line-btn").addEventListener("click", function () {
+      // 파일 나누기
+      let slices = [];
+      for (let i = 0; i < files.length; i++) {
+        let file = files[i];
+        let img = document.createElement("img");
+        img.src = URL.createObjectURL(file);
+        img.onload = function () {
+          let canvas = document.createElement("canvas");
+          canvas.width = img.width;
+          canvas.height = img.height;
+          let ctx = canvas.getContext("2d");
+          ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height);
+        
+          let sliceWidth = img.width / 4;
+          let sliceHeight = img.height / 4;
+        
+          for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 4; j++) {
+              let sliceCanvas = document.createElement("canvas");
+              sliceCanvas.width = 370;
+              sliceCanvas.height = 320;
+              let sliceCtx = sliceCanvas.getContext("2d");
+              // Add transparent pixels to the left and right of each slice
+              sliceCtx.fillStyle = "rgba(0, 0, 0, 0)";
+              sliceCtx.fillRect(0, 0, 25, 320);
+              sliceCtx.fillRect(345, 0, 25, 320);
+              sliceCtx.drawImage(canvas, j * sliceWidth, i * sliceHeight, sliceWidth, sliceHeight, 25, 0, 320, 320);
+              slices.push(sliceCanvas.toDataURL("image/png"));
+            }
+          }
+        
+          // Add a 740*640px PNG file to the slices array
+          let fullCanvas = document.createElement("canvas");
+          fullCanvas.width = 370;
+          fullCanvas.height = 320;
+          let fullCtx = fullCanvas.getContext("2d");
+          fullCtx.drawImage(canvas, 0, 0, canvas.width, canvas.height, 25, 0, 320, 320);
+        
+          if (slices.length == 16) {
+            // zip 파일 만들기
+            let zip = new JSZip();
+            for (let k = 0; k < slices.length; k++) {
+              zip.file((k + 1) + ".png", slices[k].split(",")[1], { base64: true });
+            }
+            zip.generateAsync({ type: "blob" }).then(function (blob) {
+              saveAs(blob, "naver_band_sticker.zip");
+            });
+          }
+        };
+      }
+    });
+
+    // 왓스티커 버튼 클릭 시
+  document.querySelector("#kakao-btn").addEventListener("click", function () {
     // 파일 나누기
     let slices = [];
     for (let i = 0; i < files.length; i++) {
@@ -148,8 +256,8 @@ function handleFileDrop(files) {
         for (let i = 0; i < 4; i++) {
           for (let j = 0; j < 4; j++) {
             let sliceCanvas = document.createElement("canvas");
-            sliceCanvas.width = 360;
-            sliceCanvas.height = 360;
+            sliceCanvas.width = 512;
+            sliceCanvas.height = 512;
             let sliceCtx = sliceCanvas.getContext("2d");
             sliceCtx.drawImage(canvas, j * sliceWidth, i * sliceHeight, sliceWidth, sliceHeight, 0, 0, sliceCanvas.width, sliceCanvas.height);
             slices.push(sliceCanvas.toDataURL("image/png"));
@@ -160,64 +268,10 @@ function handleFileDrop(files) {
           // zip 파일 만들기
           let zip = new JSZip();
           for (let k = 0; k < slices.length; k++) {
-            zip.file("kakao/" + (k + 17) + ".png", slices[k].split(",")[1], { base64: true });
+            zip.file((k + 1) + ".png", slices[k].split(",")[1], { base64: true });
           }
           zip.generateAsync({ type: "blob" }).then(function (blob) {
-            saveAs(blob, "카카오톡(2).zip");
-          });
-        }
-      };
-    }
-  });
-
-  // 두번째 네이버 OGQ 버튼 클릭 시
-  document.querySelector("#naver-btn2").addEventListener("click", function () {
-    // 파일 나누기
-    let slices = [];
-    for (let i = 0; i < files.length; i++) {
-      let file = files[i];
-      let img = document.createElement("img");
-      img.src = URL.createObjectURL(file);
-      img.onload = function () {
-        let canvas = document.createElement("canvas");
-        canvas.width = img.width;
-        canvas.height = img.height;
-        let ctx = canvas.getContext("2d");
-        ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height);
-      
-        let sliceWidth = img.width / 4;
-        let sliceHeight = img.height / 4;
-      
-        for (let i = 0; i < 4; i++) {
-          for (let j = 0; j < 4; j++) {
-            let sliceCanvas = document.createElement("canvas");
-            sliceCanvas.width = 740;
-            sliceCanvas.height = 640;
-            let sliceCtx = sliceCanvas.getContext("2d");
-            // Add transparent pixels to the left and right of each slice
-            sliceCtx.fillStyle = "rgba(0, 0, 0, 0)";
-            sliceCtx.fillRect(0, 0, 50, 640);
-            sliceCtx.fillRect(690, 0, 50, 640);
-            sliceCtx.drawImage(canvas, j * sliceWidth, i * sliceHeight, sliceWidth, sliceHeight, 50, 0, 640, 640);
-            slices.push(sliceCanvas.toDataURL("image/png"));
-          }
-        }
-      
-        // Add a 740*640px PNG file to the slices array
-        let fullCanvas = document.createElement("canvas");
-        fullCanvas.width = 740;
-        fullCanvas.height = 640;
-        let fullCtx = fullCanvas.getContext("2d");
-        fullCtx.drawImage(canvas, 0, 0, canvas.width, canvas.height, 50, 0, 640, 640);
-      
-        if (slices.length == 16) {
-          // zip 파일 만들기
-          let zip = new JSZip();
-          for (let k = 0; k < slices.length; k++) {
-            zip.file("ogq/" + (k + 17) + ".png", slices[k].split(",")[1], { base64: true });
-          }
-          zip.generateAsync({ type: "blob" }).then(function (blob) {
-            saveAs(blob, "네이버OGQ(2).zip");
+            saveAs(blob, "Whatsticker_sticker.zip");
           });
         }
       };
